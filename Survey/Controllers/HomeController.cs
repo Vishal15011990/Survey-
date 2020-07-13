ff3e83033dff59003626fa09c6684cbb9ba93467
Vishal Kumar <kumar.preparestudy@outlook.com>
2020-07-13 11:26:54 +0530

    Add project files.

---
+++ Survey/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login()
+        {
+            return Json("Success");
+        }
+    }
+}
